6a4e87a448a79193a6dfb97dfa3c0cef44e0d709
Daniel Porteous <danielporteous1@gmail.com>
2016-09-08 03:58:37 +1000

    Improved commenting and removed redundant code.

--- Proj1.hs
+++ Proj1.hs
@@ -4,18 +4,30 @@ import Card
 import Data.List
 import qualified Data.Set as Set
 
+----
+-- GameState type. TODO is this a type?
+----
+
 data GameState = GameState {
-    numGuesses :: Int,
-    numCards :: Int,
-    optionsSpace :: [[Card]],
-    won :: Bool -- True if won, false otherwise.
+    numCards :: Int, -- Might not be necessary.
+    optionsSpace :: [[Card]]
 } deriving (Show)
 
--- Answer -> Guess -> Feedback
+
+----
+-- Main function prototypes.
+----
 feedback :: [Card] -> [Card] -> (Int,Int,Int,Int,Int)
 initialGuess :: Int -> ([Card],GameState)
 -- Takes guess and gamestate, and the feedback, and produces a new guess and gamestate.
 nextGuess :: ([Card],GameState) ->(Int,Int,Int,Int,Int) -> ([Card],GameState)
+
+
+
+
+----
+-- Code for the feedback function.
+----
 
 -- Intersect without duplicates.
 -- https://stackoverflow.com/questions/27332815/haskell-intersection-with-duplicates
@@ -39,11 +51,43 @@ feedback answer guess = (correct, lower, sameRank, higher, sameSuit) where
     -- 5. Get same suits.
     sameSuit = length $ intersectNoDuplicates [suit s | s <- answer] [suit s | s <- guess] -- todo verify that these actually work.
 
+
+
+
+----
+-- Code for the initialGuess function.
+----
+
+-- Helper for getOptionsSpace. Ensures that all elements in a list are unique.
+-- https://stackoverflow.com/questions/31036474/haskell-checking-if-all-list-elements-are-unique
+allDifferent :: (Eq a) => [a] -> Bool
+allDifferent list = case list of
+    []      -> True
+    (x:xs)  -> x `notElem` xs && allDifferent xs
+
+-- Efficient implementation of nub.
+nubOrd :: Ord a => [a] -> [a] 
+nubOrd xs = go Set.empty xs where
+  go s (x:xs)
+   | x `Set.member` s = go s xs
+   | otherwise        = x : go (Set.insert x s) xs
+  go _ _              = []
+
+-- Enumerates all the possible guesses at the start of the game.
+-- Works for any number of cards and watches out for duplicate and invalid guesses.
+getOptionsSpace :: Int -> [[Card]]
+getOptionsSpace i = nubOrd [sort x | x <- sequence $ replicate i cards, allDifferent x]
+    where
+    cards = [(Card Club R2) .. (Card Spade Ace)]
+
+-- Takes every nth element of a list:
+-- https://stackoverflow.com/questions/2026912/how-to-get-every-nth-element-of-an-infinite-list-in-haskell
 every n xs = case drop (n-1) xs of
               (y:ys) -> y : every n ys
               [] -> []
 
-
+-- Creates an optimal initial guess. This means each card has a different suit and the ranks
+-- are equidistant from each other based on the number of cards in the guess (13/i).
 initialGuess 0 = error "Need at least 1 card"
 initialGuess i = (cards, state)
     where
@@ -53,7 +97,28 @@ initialGuess i = (cards, state)
         suits = take i [Club ..]
         ranks = take i $ every (13 `div` i) [R2 ..]
     -- The space of options is all cards except for those already guessed.
-    state = GameState {numGuesses=0, numCards=i, optionsSpace=(getOptionsSpace i) \\ [cards], won=False}
+    state = GameState {numCards=i, optionsSpace=(getOptionsSpace i) \\ [cards]}
+
+
+
+
+----
+-- Code for the nextGuess function.
+----
+
+-- Currently just keeps taking the next possible guess until we get it right.
+nextGuess (prevGuess, state) feedback = (newGuess, newState)
+    where
+    newGuess = head $ optionsSpace state
+    updateState s = s {optionsSpace = optionsSpace s \\ [newGuess]}
+    newState = updateState state
+
+
+
+
+
+
+
 
 -- One way of maybe getting all possible guesses:
 -- [x | x <- subsequences [(Card Club R2) .. (Card Spade Ace)], length x == 2]
@@ -69,28 +134,3 @@ initialGuess i = (cards, state)
 -- Finally call it with this:
 -- cartProd [(Card Club R2) .. (Card Spade Ace)] [(Card Club R2) .. (Card Spade Ace)]
 -- This only currently works for guesses of length 2.
-
-allDifferent :: (Eq a) => [a] -> Bool
-allDifferent list = case list of
-    []      -> True
-    (x:xs)  -> x `notElem` xs && allDifferent xs
-
-nubOrd :: Ord a => [a] -> [a] 
-nubOrd xs = go Set.empty xs where
-  go s (x:xs)
-   | x `Set.member` s = go s xs
-   | otherwise        = x : go (Set.insert x s) xs
-  go _ _              = []
-
-getOptionsSpace :: Int -> [[Card]]
-getOptionsSpace i = nubOrd [sort x | x <- sequence $ replicate i cards, allDifferent x]
-    where
-    cards = [(Card Club R2) .. (Card Spade Ace)]
-
-
-
-nextGuess (prevGuess, state) feedback = (newGuess, newState)
-    where
-    newGuess = head $ optionsSpace state
-    updateState s = s {optionsSpace = optionsSpace s \\ [newGuess]}
-    newState = updateState state
