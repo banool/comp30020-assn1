610183c36d70dd14d943ab94d7a58102b1bbd954
Daniel Porteous <danielporteous1@gmail.com>
2016-09-11 19:41:18 +1000

    Cleaned up code style and improved commenting.

--- Proj1.hs
+++ Proj1.hs
@@ -1,17 +1,28 @@
+-- File     : Proj1.hs
+-- Author   : Daniel Porteous
+-- Purpose  : Implement guessing card game for project 1
+
+-- Note: Links to stackoverflow indicate where inspiration for the code came 
+--       from. The code that appears here was written by me and will likely 
+--       look different.
+
 module Proj1 (feedback, initialGuess, nextGuess, GameState) where
 
 import Card
 import Data.List
 import qualified Data.Set as Set
 
+
 ----
--- GameState record.
+-- GameState record and global constants.
 ----
 
 data GameState = GameState {
     numCards :: Int, -- Might not be necessary.
     optionsSpace :: [[Card]]
 } deriving (Show)
+
+cardsPerSuit = 13
 
 
 ----
@@ -22,8 +33,6 @@ initialGuess :: Int -> ([Card],GameState)
 nextGuess :: ([Card],GameState) -> (Int,Int,Int,Int,Int) -> ([Card],GameState)
 
 
-
-
 ----
 -- Code for the feedback function.
 ----
@@ -32,13 +41,14 @@ nextGuess :: ([Card],GameState) -> (Int,Int,Int,Int,Int) -> ([Card],GameState)
 -- https://stackoverflow.com/questions/27332815/haskell-intersection-with-duplicates
 intersectNoDuplicates xs ys = xs \\ (xs \\ ys)
 
--- Takes the answer and guess and returns feedback. All 5 parts of the feedback are implemented
--- in this function body, none were complicated enough to require their own functions.
+-- Takes the answer and guess and returns feedback. All 5 parts of the feedback 
+-- are implemented in this function body, none were complicated enough to 
+-- require their own functions.
 feedback answer guess = (correct, lower, sameRank, higher, sameSuit) where
     -- 1. Get all the correct guesses.
     correct = length $ intersect guess answer
 
-    -- 2/3. Get ranks lower than lowest in guess and ranks higher than highest in guess.
+    -- 2/3. Get ranks lower than lowest in guess and ranks higher than highest.
     _guessRanks  = sort [rank r | r <- guess]
     _answerRanks = sort [rank r | r <- answer]
 
@@ -52,8 +62,6 @@ feedback answer guess = (correct, lower, sameRank, higher, sameSuit) where
     sameSuit = length $ intersectNoDuplicates [suit s | s <- answer] [suit s | s <- guess]
 
 
-
-
 ----
 -- Code for the initialGuess function.
 ----
@@ -61,13 +69,12 @@ feedback answer guess = (correct, lower, sameRank, higher, sameSuit) where
 -- Helper for getOptionsSpace. Ensures that all elements in a list are unique.
 -- https://stackoverflow.com/questions/31036474/haskell-checking-if-all-list-elements-are-unique
 allDifferent :: (Eq a) => [a] -> Bool
-allDifferent list = case list of
-    []      -> True
-    (x:xs)  -> x `notElem` xs && allDifferent xs
-
+allDifferent [] = True
+allDifferent (x:xs) = x `notElem` xs && allDifferent xs
 
 -- Efficient implementation of nub.
 -- https://hackage.haskell.org/package/extra-1.5/docs/Data-List-Extra.html
+-- This is just for time improvements, regular nub could just as easily be used.
 nubOrd :: Ord a => [a] -> [a] 
 nubOrd xs = go Set.empty xs where
   go s (x:xs)
@@ -75,45 +82,43 @@ nubOrd xs = go Set.empty xs where
    | otherwise        = x : go (Set.insert x s) xs
   go _ _              = []
 
-
 -- Enumerates all the possible guesses at the start of the game.
--- Works for any number of cards and watches out for duplicate and invalid guesses.
+-- Works for any number of cards and checks for duplicate and invalid guesses.
 getOptionsSpace :: Int -> [[Card]]
 getOptionsSpace i = nubOrd [sort x | x <- sequence $ replicate i cards, allDifferent x]
     where
     -- cards = [(Card Club R2) ..] should work, but doesn't for some reason.
     cards = [(Card Club R2) .. (Card Spade Ace)]
 
-
 -- Takes every nth element of a list:
 -- https://stackoverflow.com/questions/2026912/how-to-get-every-nth-element-of-an-infinite-list-in-haskell
+-- The drop function returns the latter part of the list starting from n.
 every n xs = case drop (n-1) xs of
-              (y:ys) -> y : every n ys
-              [] -> []
+            (y:ys)  -> y : every n ys
+            []      -> []
 
-
--- Creates an optimal initial guess. This means that each card has a different suit and the
--- ranks are equidistant from each other based on the number of cards in the guess (13/n).
--- Only need to consider up to 4 cards.
+-- Creates an optimal initial guess. This means that each card has a different 
+-- suit and the ranks are equidistant from each other based on the number of 
+-- cards in the guess (13/n). Only need to consider up to 4 cards.
 initialGuess 0 = error "Need at least 1 card"
 initialGuess i = (cards, state)
     where
     cards = zipWith Card suits ranks
         where
         suits = take i [Club ..]
-        ranks = take i $ every (13 `div` i) [R2 ..]
+        ranks = take i $ every (cardsPerSuit `div` i) [R2 ..]
     -- The space of options is all cards except for those already guessed.
     state = GameState {numCards=i, optionsSpace=(getOptionsSpace i) \\ [cards]}
-
-
 
 
 ----
 -- Code for the nextGuess function.
 ----
 
--- Returns a new options space without cards that can't be in the answer based on
+-- Returns a new optionsSpace without cards that can't be in the answer based on
 -- removing cards than are lower than the lowest rank, vice versa for highest.
+-- This function reduces the space by far less than the getKnuthSpace function,
+-- if at all, but is a demonstration of a more bulky function.
 getSpaceNoLowerHigher :: ([Card], GameState) -> (Int,Int,Int,Int,Int) -> [[Card]]
 getSpaceNoLowerHigher (prevGuess, state) fb = newSpace
     where
@@ -124,47 +129,63 @@ getSpaceNoLowerHigher (prevGuess, state) fb = newSpace
     _lower  = (\(_,a,_,_,_) -> a) fb
     _higher = (\(_,_,_,a,_) -> a) fb
 
+    _prevSpace = optionsSpace state
+
     -- If no cards in the answer are lower rank than the lowest in the previous 
     -- guess, remove all guesses from the options space that have a rank lower 
     -- than the lowest in the previous guess.
     _noLower = 
         if _lower == 0
-            -- Keep an option in the list if none of the cards are lower than the lowest.
-            then [option | option <- optionsSpace state, not $ any (\c -> (rank c) < _lowestRank) [c | c <- option]]
-            else optionsSpace state
+            -- Keep an option in the optionsSpace list only if none of the 
+            -- cards are lower than the lowest.
+            then [option | option <-  _prevSpace, 
+                not $ any (\c -> (rank c) < _lowestRank) [c | c <- option]]
+            else  _prevSpace
     _noHigher = 
         if _higher == 0
-            then [option | option <- _noLower, not $ any (\c -> (rank c) > _highestRank) [c | c <- option]]
-            else optionsSpace state
+            then [option | option <- _noLower, 
+                not $ any (\c -> (rank c) > _highestRank) [c | c <- option]]
+            else _noLower
 
     newSpace = _noHigher
 
-
--- Applies the algorithm for mastermind described here, except obviously for this game:
+-- Applies the algorithm for mastermind option space reduction described here:
 -- https://math.stackexchange.com/questions/1192961/knuths-mastermind-algorithm
 -- It works like this:
 --     Go through the possible guess space.
 --     For each possibility, treat it if it were the answer.
---     Check if you get the same feedback with this as the answer compared to the previous guess.
---     If you don't get the same response, this possibility cannot be the answer.
+--     Check if you get the same feedback with this as the answer compared to 
+--     the previous guess. If you don't get the same response, this 
+--     possibility cannot be the answer.
 getKnuthSpace :: ([Card], GameState) -> (Int,Int,Int,Int,Int) -> [[Card]]
 getKnuthSpace (prevGuess, state) fb = newSpace
     where
     newSpace = [option | option <- optionsSpace state, feedback option prevGuess == fb]
 
 
--- Takes a gamestate and returns it with the new optionsSpace applied.
+-- Takes a gamestate and optionsSpace and returns it with the new optionsSpace.
 updateStateSpace :: GameState -> [[Card]] -> GameState
 updateStateSpace gs newSpace = gs {optionsSpace = newSpace}
 
 
--- Currently just keeps taking the next possible guess until we get it right.
+-- This function applies a series of steps to the space before deciding upon
+-- a new guess. It would be nice to do point free style, but the return types
+-- of the above functions make it difficult to implement elegantly.
+-- As such, the function creates intermediate spaces which track changes
+-- throughout moving from the previous state to the new state. 
+-- This is good from a diagnostics standpoint because its easy to check 
+-- what changes with each operation.
 nextGuess (prevGuess, state) fb = (newGuess, newState)
     where
-    -- 2/3. Get ranks lower than lowest in guess and ranks higher than highest in guess.
+    
+    -- Remove the previous guess from the state since it has been checked.
     _intermediateSpace1 = optionsSpace state \\ [prevGuess]
+
+    -- Get ranks lower than lowest in guess and ranks higher than highest.
     _intermediateSpace2 = getSpaceNoLowerHigher (prevGuess, updatedSpace) fb
-        where updatedSpace = updateStateSpace state _intermediateSpace1 
+        where updatedSpace = updateStateSpace state _intermediateSpace1
+
+    -- Apply the Knuth algorithm for optionsSpace reduction.
     _intermediateSpace3 = getKnuthSpace (prevGuess, updatedSpace) fb
         where updatedSpace = updateStateSpace state _intermediateSpace2
 
